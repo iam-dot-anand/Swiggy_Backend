04a7b6b1e8891887105d571caaf23e61dc2a2268
Anand <anandvishwakarma0052@gmail.com>
2025-07-08 23:47:54 +0530

    remove console

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const authRouter = require('./Routes/authRouter');
-const product = require('./Routes/productRouter');
-const restaurant = require('./Routes/restaurantRouter');
-const { connectMongoDB } = require('./MongoDBConn');
-require('dotenv').config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const authRouter = require("./Routes/authRouter");
+const product = require("./Routes/productRouter");
+const restaurant = require("./Routes/restaurantRouter");
+const { connectMongoDB } = require("./MongoDBConn");
+require("dotenv").config();
 
 const app = express();
-const PORT=process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 // MongoDB Connection
-connectMongoDB('mongodb+srv://kr:krishna123@swiggy-db.lp3w8.mongodb.net/?retryWrites=true&w=majority&appName=swiggy-db',{ useNewUrlParser: true, useUnifiedTopology: true })
+connectMongoDB(
+  "mongodb+srv://kr:krishna123@swiggy-db.lp3w8.mongodb.net/?retryWrites=true&w=majority&appName=swiggy-db",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
   .then(() => console.log("MongoDB Connected!"))
-  .catch(err => console.log("Error in MongoDB: " + err));
+  .catch((err) => console.log("Error in MongoDB: " + err));
 
-// Middleware 
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
-app.use('/auth', authRouter);
-app.use('/product', product);
-app.use('/restaurant', restaurant);
+app.use("/auth", authRouter);
+app.use("/product", product);
+app.use("/restaurant", restaurant);
 
 app.listen(PORT, () => {
   console.log(`Server is started! at http://localhost:${PORT}`);
